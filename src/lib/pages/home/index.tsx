1448fbe34354e63674fc9185918c21bf5717e404
Jonathan Nuñez <37056901+JonathanNunezRdz@users.noreply.github.com>
2022-09-05 10:29:48 -0500

    refactor(reset): desktop reset

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,11 +1,14 @@
 import { Box } from '@chakra-ui/react';
+import { NextSeo } from 'next-seo';
+import type { FC } from 'react';
 
 import SomeImage from 'lib/components/samples/SomeImage';
 import SomeText from 'lib/components/samples/SomeText';
 
-const Home = () => {
+const Home: FC = () => {
 	return (
 		<Box display={{ md: 'flex' }} alignItems='center' minHeight='60vh' gap={8} mb={8} w='full'>
+			<NextSeo title='Home' />
 			<SomeImage />
 			<SomeText />
 		</Box>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,11 +30,11 @@ class MyDocument extends Document {
 
 					{/* add your own app-icon */}
 					{/* <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/icons/apple-touch-icon.png"
-          />
-          <link rel="icon" href="/app-icon.png" /> */}
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/icons/apple-touch-icon.png"
+					/>
+					<link rel="icon" href="/app-icon.png" /> */}
 					<link rel='manifest' href='/manifest.json' />
 				</Head>
 				<body>
